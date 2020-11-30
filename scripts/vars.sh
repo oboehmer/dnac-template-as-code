6bd6d60d5f5535da3f5ac761caad9d763ae78fc7
Oliver Böhmer <oboehmer@cisco.com>
2020-11-30 17:50:29 +0000

    Disable debug output

--- scripts/vars.sh
+++ scripts/vars.sh
@@ -3,6 +3,8 @@
 # setup vars for cicd pipeline
 
 TEMPLATE_DIR="dnac-templates/"
+DEBUG=""
+# DEBUG="--debug"
 
 if [ "$CI_BUILD_REF_NAME" == "master" ] ; then 
     CONFIG_YAML="scripts/config.yaml"
@@ -15,6 +17,7 @@ else
 fi
 
 cat << _EOF
+DEBUG=$DEBUG
 CONFIG_YAML=$CONFIG_YAML
 TEMPLATE_DIR=$TEMPLATE_DIR
 DEPLOY_DIR=$DEPLOY_DIR
